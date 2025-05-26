16ac412289eb491bdc8f68ec5495c29c7e47c4b8
Vlad <example@mail.com>
2025-05-26 15:26:14 +0300

    add skeleton for news and banner

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,16 +3,16 @@ import axios from "axios"
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews = async () => {
-    try {
-        const resp = await axios.get(`${BASE_URL}latest-news`, {
-            params: {
-                apiKey: API_KEY
-            }
-        })
-        return resp.data
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+// export const getNews = async () => {
+//     try {
+//         const resp = await axios.get(`${BASE_URL}latest-news`, {
+//             params: {
+//                 apiKey: API_KEY
+//             }
+//         })
+//         return resp.data
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
