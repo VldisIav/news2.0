64ed83250fb4485b823c7a575b619ec6d53c265b
Vlad <example@mail.com>
2025-05-26 15:57:17 +0300

    add pagination!

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,35 +4,56 @@ import styles from './styles.module.css';
 import { getNews } from '../../api/apiNews';
 import NewsList from '../../components/NewsList/NewsList';
 import Skeleton from '../../components/Skeleton/Skeleton';
+import Pagination from '../../components/Pagination/Pagination';
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [error, setError] = useState(null);
   const [isloading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
+
+  const fetchNews = async (currentPage) => {
+    try {
+      const response = await getNews(currentPage, pageSize);
+      if (response && response.news) {
+        setIsLoading(true);
+        setNews(response.news);
+        console.log(response.news);
+        setIsLoading(false);
+      } else {
+        setError('Invalid data format from API');
+        console.error('Invalid data format from API', response);
+      }
+    } catch (err) {
+      setError(err.message); // Обработка ошибок запроса
+      console.error('Error fetching news:', err);
+    }
+  };
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await getNews();
-        if (response && response.news) {
-          setIsLoading(true);
-          setNews(response.news);
-          console.log(response.news);
-          setIsLoading(false);
-        } else {
-          setError('Invalid data format from API'); // Обработка неверного формата данных
-          console.error('Invalid data format from API', response);
-        }
-      } catch (err) {
-        setError(err.message); // Обработка ошибок запроса
-        console.error('Error fetching news:', err);
-      }
-    };
-    fetchNews();
-  }, []);
+    fetchNews(currentPage);
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   if (error) {
-    return <div className={styles.error}>Error: {error}</div>; // Отображение сообщения об ошибке
+    return <div className={styles.error}>Error: {error}</div>;
   }
 
   return (
@@ -42,7 +63,23 @@ const Main = () => {
       ) : (
         <Skeleton count={1} type="banner" />
       )}
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        handlePageClick={handlePageClick}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
+
       {!isloading ? <NewsList news={news} /> : <Skeleton count={10} type="item" />}
+
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        handlePageClick={handlePageClick}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
     </main>
   );
 };
